8cc2b8e4fe0204d176c0418acfb5478795cb995f
Patrick Wertal <patrick.wertal@gmail.com>
2022-03-18 21:16:08 +0100

    added Signout

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import {Container, Row ,Col,Navbar,Nav} from 'react-bootstrap' ;
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+import { withAuthenticator ,AmplifySignOut} from '@aws-amplify/ui-react';
+
 Amplify.configure(awsconfig);
 
 
-function App() {
+function App({}) {
 
   let users : string[] = ["Patrick Wertal","Helge Folkmann"] ;
 
@@ -21,8 +21,7 @@ function App() {
 
    },[])
 
-   
-
+  
   return (
     <Container fluid className='fullsize'>
         <Navbar sticky="top" bg="light" variant="light">
@@ -37,14 +36,20 @@ function App() {
     </Nav>
     </Container>
   </Navbar>
-        <Row>
+        <Row justify-content-center h-100>
         <Col lg={6} md={6} xs={12}>
-          { users && users.map( (user :string ) =>   <div style={{ background: "red" }}> {user}</div>  )}
+          <div  className="leftSide"> 
+          { users && users.map( (user :string ) =>   <div style={{ background: "#ff9900" }}> {user}</div>  )}
+          </div>
         </Col>
-        <Col lg={6} md={6} xs={12}>
-          <div style={{ background: "blue" }}>BERN</div>
+        <Col lg={6} md={6} xs={12} >
+        <div className="rightSide" ></div>
         </Col>
       </Row>
+      <div className="footer">
+      <AmplifySignOut></AmplifySignOut>
+    </div>
+     
     </Container>
 
   );
